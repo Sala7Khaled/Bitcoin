3009fd824dedbd20fe51d9372dadacdb9ecd291c
S7K <sala7khaled.s7k@gmail.com>
2020-03-07 19:31:38 +0200

    first commit

---
+++ Bitcoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  Bitcoin
+//
+//  Created by Salah Khaled on 3/7/20.
+//  Copyright © 2020 Salah Khaled. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable{
+    let rate: Double
+}
